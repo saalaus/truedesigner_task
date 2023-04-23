0da7845288c9f83bcd71888fc9b13d0cf6d37dcf
saalaus <saalaus2@gmail.com>
2023-04-23 18:41:00 +0500

    change base for build

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+    base: "/truedesigner_task/",
     build: {
         rollupOptions: {
             input: {
