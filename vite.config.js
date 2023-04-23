bdc425228dd346e35ad372e55df65d2bc38a4a46
saalaus <saalaus2@gmail.com>
2023-04-23 18:35:28 +0500

    fix build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
-import path from "path";
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
-export default {
-    root: "./src",
-    base: "/",
-    assetsDir: "assets",
+export default defineConfig({
     build: {
-        entry: "./index.html",
+        rollupOptions: {
+            input: {
+                index: resolve(__dirname, "index.html"),
+                description: resolve(
+                    __dirname,
+                    "src",
+                    "2_description",
+                    "index.html"
+                ),
+                datetime: resolve(__dirname, "src", "3_datetime", "index.html"),
+                map: resolve(__dirname, "src", "4_map", "index.html"),
+                photo: resolve(__dirname, "src", "5_photo", "index.html"),
+                finish: resolve(__dirname, "src", "6_finish", "index.html"),
+            },
+        },
     },
-};
+});
